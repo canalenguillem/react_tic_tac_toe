4797ec19ca2dc1893ec06dea8cdaa785e3217691
Tu Nombre <you@example.com>
2024-07-28 07:27:00 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Board from "./components/Board";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Tres en Raya</h1>
+      <div>TABLERO</div>
+    </div>
+  );
+};
+
+export default App;
